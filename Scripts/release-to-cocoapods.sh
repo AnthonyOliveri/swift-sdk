1f959c3804fdde393bcdda6ba1daf31fdc37bf8e
Anthony Oliveri <oliveri@us.ibm.com>
2018-09-27 14:06:11 -0500

    Repurpose release script to only publish to Cocoapods

---
+++ Scripts/release-to-cocoapods.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Publishes the latest version of all services to Cocoapods
+
+declare -a allPods=(
+  "IBMWatsonAssistantV1.podspec"
+  "IBMWatsonConversationV1.podspec"
+  "IBMWatsonDiscoveryV1.podspec"
+  "IBMWatsonLanguageTranslatorV3.podspec"
+  "IBMWatsonNaturalLanguageClassifierV1.podspec"
+  "IBMWatsonNaturalLanguageUnderstandingV1.podspec"
+  "IBMWatsonPersonalityInsightsV3.podspec"
+  "IBMWatsonSpeechToTextV1.podspec"
+  "IBMWatsonTextToSpeechV1.podspec"
+  "IBMWatsonToneAnalyzerV3.podspec"
+  "IBMWatsonVisualRecognitionV3.podspec"
+)
+
+for podspec in "${allPods[@]}"
+do
+  # This will only publish pods if their version has been updated
+  # pod trunk push $podspec --allow-warnings
+  echo "Pushing ${podspec} to Cocoapods!"
+done
