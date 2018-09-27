efbc0e97784ea5752b12767057229ef2d71a3d41
Anthony Oliveri <oliveri@us.ibm.com>
2018-09-27 16:58:48 -0500

    Fix automated documentation publishing

--- Scripts/publish-documentation.sh
+++ Scripts/publish-documentation.sh
@@ -3,7 +3,7 @@
 # Generate API docs and upload them to Github on the gh-pages branch
 
 ./Scripts/generate-documentation.sh
-git checkout -b gh-pages origin/gh-pages
+git checkout --track origin/gh-pages
 cp -r gh-pages/* .
 rm -rf gh-pages/
 git clean -df
