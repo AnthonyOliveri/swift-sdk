9e8922115a7c870d8e94f0451a3dc29b22f941d2
Anthony Oliveri <oliveri@us.ibm.com>
2018-09-27 15:25:06 -0500

    fix(docs): Automate API docs publishing
    Add doc publishing to Travis deploy phase

---
+++ Scripts/publish-documentation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Generate API docs and upload them to Github on the gh-pages branch
+
+./generate-documentation.sh
+git checkout gh-pages
+cp -r gh-pages/* .
+rm -rf gh-pages/
+git clean -df
+git add .
+# Get the release version from the latest git tag
+git commit -m "SDK docs for release $(git describe --abbrev=0 --tags)"
+git push
