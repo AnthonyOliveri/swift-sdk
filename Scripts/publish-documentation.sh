7e08339171b0dccab162adc4defc8d26ef2a8e07
Anthony Oliveri <oliveri@us.ibm.com>
2018-09-27 15:39:00 -0500

    fix(automation): publish docs script

--- Scripts/publish-documentation.sh
+++ Scripts/publish-documentation.sh
@@ -2,12 +2,21 @@
 
 # Generate API docs and upload them to Github on the gh-pages branch
 
-./generate-documentation.sh
+echo "1"
+sh ./generate-documentation.sh
+echo "2"
 git checkout gh-pages
+echo "3"
 cp -r gh-pages/* .
+echo "4"
 rm -rf gh-pages/
+echo "5"
 git clean -df
+echo "6"
 git add .
+echo "7"
 # Get the release version from the latest git tag
 git commit -m "SDK docs for release $(git describe --abbrev=0 --tags)"
+echo "8"
 git push
+echo "9"
