160f23e1a12ca400a7f6b667a6c6c09cf5ac3d75
Anthony Oliveri <oliveri@us.ibm.com>
2019-01-04 11:48:01 -0600

    feat(docs): Alter documentation publishing to work on Jenkins

--- Scripts/publish-documentation.sh
+++ Scripts/publish-documentation.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 # Generate API docs and upload them to Github on the gh-pages branch
-# IMPORTANT: This is only meant to be called from Travis builds!
+# IMPORTANT: This is only meant to be called from Jenkins!
 
 set -e
 
-# Configure Travis to be able to push to the Github repo
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+# Configure Jenkins to be able to push to the Github repo
+git config --global user.name "Jenkins"
 git remote rm origin
-git remote add origin https://watson-developer-cloud:${GH_TOKEN}@github.com/watson-developer-cloud/swift-sdk.git
+# git remote add origin https://watson-developer-cloud:${GH_TOKEN}@github.com/watson-developer-cloud/swift-sdk.git
+git remote add origin https://AnthonyOliveri:${GH_TOKEN}@github.com/watson-developer-cloud/swift-sdk.git
 git fetch
 git fetch --tags
 latestVersion=$(git describe --abbrev=0 --tags)
